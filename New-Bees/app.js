27f6c65c2429d16805de121a8d96345b0a99fdd3
jason qian <shenggangq@student.unimelb.edu.au>
2021-08-16 16:35:04 +0800

    dependencies

--- New-Bees/app.js
+++ New-Bees/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
+const path = require('path')
+const port = process.env.PORT || 8000 
 
 app.get('/', (req, res) => {
     console.log('<h1>ththt</h1>')
