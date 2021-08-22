a5c5516d95e215021da23690090378ff40c1c4b2
Ziyuq <ziyuq@student.unimelb.edu.au>
2021-08-22 22:51:42 +1000

    build the model of user

--- New-Bees/models/user.js
+++ New-Bees/models/user.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose")
 const bcrypt = require('bcrypt-nodejs')
+const userSchema = new mongoose.Schema({
+    givenName: { type: String, required: true },
+    familyName: { type: String, required: true },
+    emailAddress: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    photo: {
+        data: Buffer,
+        contentType: String
+    }
+})
+
+const user = userSchema = mongoose.model('user', userSchema)
+module.exports = user
