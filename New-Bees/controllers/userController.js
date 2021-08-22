ba7e04e07bfb13d0dfb000ec8efc5f2c8ff761e2
jason qian <shenggangq@student.unimelb.edu.au>
2021-08-22 22:27:44 +0800

    Update userController.js

--- New-Bees/controllers/userController.js
+++ New-Bees/controllers/userController.js
@@ -1,16 +1,38 @@
-const User = require('../models/user')
-const addUser = async(req, res) => {
-    let newUser = new User();
-    newUser.givenName = req.body.givenName;
-    newUser.familyName = req.body.familyName;
-    newUser.emailAddress = req.body.emailAddress;
-    newUser.password = req.body.password;
-    newUser.save(function(err) {
-        if (err) {
-            throw err;
+// customerController will be written on next 
+const User  = require('../models/customerModel')
+
+
+const userLogin = async (req,res) => {
+	try{
+        // To check if the email registered
+        const user = await User.findOne({email: req.body.email})
+        if (user == null) { // email not found in database
+			res.status(400).json({success: false, error: "Email not registered"})
+		} else {
+            // To match the customer password
+            if (user.password == req.body.password) {
+                res.status(200).json({success: true, 
+                    user: {
+                        id: user.id,
+                        email: user.email,
+                        firstName: user.firstName,
+                        lastName: user.lastName
+                        
+                    }})
+                
+            } else {
+                res.status(409).json({success: false, error: "Password incorrect"})
+                // return res.send("Password incorrect")
+            }
         }
-        return done(null, newUser);
-    })
+	} catch (err) { // error occor
+		res.status(400)
+		return res.send("Database query failed")
+    }
 }
 
-module.exports = { addUser };
+
+
+module.exports = {
+    userLogin
+}
