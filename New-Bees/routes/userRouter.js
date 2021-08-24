ad168220d7b1676d727a50f3552086cdfb108100
Sid3W <xinyuwu000801@gmail.com>
2021-08-24 23:01:17 +0800

    front end, continuing tmr

--- New-Bees/routes/userRouter.js
+++ New-Bees/routes/userRouter.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/userController')
 const userRouter = express.Router()
 
 userRouter.post('/addUser', userController.addUser)
+//userRouter.post
 
 module.exports = userRouter
