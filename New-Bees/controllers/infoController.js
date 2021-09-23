4d35a22a429fd53f464d387be5c3f8241192d8c6
Ziyuq <ziyuq@student.unimelb.edu.au>
2021-09-24 02:29:37 +1000

    Update infoController.js

--- New-Bees/controllers/infoController.js
+++ New-Bees/controllers/infoController.js
@@ -1,32 +1,47 @@
 const User = require('../models/user')
 
 const editInfo = async (req, res) => {
-    let userid = req.params._id
     try {
+        let userid = req.params._id
         let givenName = req.body.givenName;
         let familyName = req.body.familyName;
         let password = req.body.password;
         let introduction = req.body.introduction;
-
+        let userID = req.body.userID
+        let mobile = req.body.mobile
+        let address = req.body.address
+        let email = req.body.email
         // Udpate the information that user has changed
         if (givenName) {
             await User.updateOne({ _id: userid }, { $set: { givenName: givenName } })
         }
-        if (familyName) {
+        else if (familyName) {
             await User.updateOne({ _id: userid }, { $set: { familyName: familyName } })
         }
-        if (password) {
+        else if (password) {
             await User.updateOne({ _id: userid }, { $set: { password: user.generateHash(password) } })
         }
-        if (information) {
+        else if (introduction) {
             await User.updateOne({ _id: userid }, { $set: { introduction: introduction } })
         }
+        else if (userID) {
+            await User.updateOne({ _id: userid }, { $set: { userID: userID } })
+        }
+        else if (mobile) {
+            await User.updateOne({ _id: userid }, { $set: { mobile: mobile } })
+        }
+        else if (address) {
+            await User.updateOne({ _id: userid }, { $set: { address: address } })
+        }
+        else if (email) {
+            await User.updateOne({ _id: userid }, { $set: { email: email } })
+        }
 
-        // get customer after updating
-        let user = await User.findOne({ _id: userid }, {})
-        res.status(200).json({ success: true, user })
+        // get user after updating
+        return res.status(200).json({ success: true })
 
     } catch (err) {
+        console.log(err)
         return res.status(404).json({ success: false, error: "Website cracked" })
     }
 }
