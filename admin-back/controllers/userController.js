fa27236e20a1559ef878bd473817daded3680498
Ziyuq <ziyuq@student.unimelb.edu.au>
2021-10-12 02:56:48 +1100

    fix status return

--- admin-back/controllers/userController.js
+++ admin-back/controllers/userController.js
@@ -13,9 +13,10 @@ const viewUsers = async (req, res) => {
 const banUser = async (req, res) => {
     try {
         await User.updateOne({ _id: req.body._id }, { $set: { ban: true } })
-        return res.status.json({ success: true })
+        return res.status(200).json({ success: true })
     }
     catch (err) {
+        console.log(err)
         return res.status(404).json({ success: false, error: "Web carshed" })
     }
 }
