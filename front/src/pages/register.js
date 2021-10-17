b2d4c83d5ae936199908820cd841d539f205b647
ShengjunSu <shengjuns@student.unimelb.edu.au>
2021-10-17 16:59:51 +0800

    Add logout to admin page

--- front/src/pages/register.js
+++ front/src/pages/register.js
@@ -142,7 +142,7 @@ export default function Register(props) {
 
                 </div>
                 <div className={classes.column} style={{ textAlign: 'center', paddingRight: '15vh', minHeight: '82vh' }}>
-                    <Container component="main" maxWidth="xs">
+                    <Container component="main" maxWidth="xs"style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)', borderRadius: 10, padding: ' 2vh 2vw'}}>
                         <div>
                             <Typography component="h1" variant="h1" align="center">
                                 Register
