1532816c810b5eb8c1562845000a8087c1bbb054
Yutong Sun <sunyutong798@gmail.com>
2021-10-22 22:29:36 +1100

    Improve web design for admin homepage and login

--- admin-front/src/pages/homepage.js
+++ admin-front/src/pages/homepage.js
@@ -1,10 +1,12 @@
 //import libraries
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Layout} from 'antd';
+import Button from '@material-ui/core/Button';
+import { useState } from 'react';
+import { Drawer} from 'antd';
 
 
-//homepage style 
+// homepage style 
 const useStyles = makeStyles((theme) => ({
     header: {
         display: 'flex',
@@ -40,8 +42,8 @@ const useStyles = makeStyles((theme) => ({
 
     },
     button: {
-        width: "250px",
-        height: "50px",
+        width: "140px",
+        height: "40px",
         background: '#429CEF',
         borderRadius: '100px',
         border: 0,
@@ -54,13 +56,14 @@ const useStyles = makeStyles((theme) => ({
         overflow: 'hidden',
         width: '100%',
         height: '100%',
-        backgroundImage: 'url("/../pics/ink1.jpeg")',
+        backgroundImage: 'url("/../pics/beach.jpeg")',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         resizeMethod: 'cover',
         position: 'absolute',
         bottom: '0',
-    }
+      
+    },
 
 }));
 
@@ -69,47 +72,138 @@ const useStyles = makeStyles((theme) => ({
 function Homepage() {
 
     const classes = useStyles();
-    const {Content} = Layout;
+    const [about, setAbout] = useState(false);
+    const [support, setSupport] = useState(false);
+    const [admin, setAdmin] = useState(false);
+    const [team, setTeam] = useState(false);
+    
+
+    const showAbout = () => {
+        setAbout(true);
+    };
+
+    const showSupport = () => {
+        setSupport(true);
+    };
+
+    const showAdmin = () => {
+        setAdmin(true);
+    };
+
+    const showTeam = () => {
+        setTeam(true);
+    };
+
+
+    const onClose = () => {
+        setAbout(false);
+        setSupport(false);
+        setAdmin(false);
+        setTeam(false);
+    };
 
     return (
+        <div style={{width: '100%', height: '100%', backgroundImage: 'url("/../pics/background18.jpg")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', position: 'absolute', right: '0vw', top:'0vh'}}>
+            {/* logo div */}
+            <div style={{position: 'absolute', left: '3vw', top:'3vh' }}>
+                <img src='./pics/logo_full.png' alt='logo pic' height={50}/>
+            </div>
 
+            {/* help divs */}
+            <div style={{position: 'absolute', right: '28.5vw', top:'3.8vh' }}>
+                <h2 onClick = {showAbout} style={{color: 'white', fontFamily: 'Ubuntu'}}> About </h2>      
+            </div>
             
-        <Layout className={classes.background} style={{ padding: '2vh 2vh', paddingRight: '2vh' }}>
-            <Content>
-                <div align="center">
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <img src='./pics/logo_full.png' alt='logo pic' height={100}/>
-                <br/>
-                <br/>
-                <img src='./pics/personalCRM.png' alt='crm pic' height={25}/>
-                <br/>
-                <br/>
-                <img src='./pics/adminversion.png' alt='adminversion pic' height={15}/>
-                
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/><br/><br/><br/><br/><br/>
-                    <a href={window.location.href + 'login'}>
-                        <img src='./pics/adminportal.png' alt='login pic' height={40}/>
-                    </a>
-                </div>
-            </Content>
-        </Layout>
+            <div style={{position: 'absolute', right: '23vw', top:'3.8vh' }}>
+                <h2 onClick = {showSupport} style={{color: 'white', fontFamily: 'Ubuntu'}}> Support </h2>              
+            </div>
+            <div style={{position: 'absolute', right: '18vw', top:'3.8vh' }}>
+                <h2 onClick = {showAdmin} style={{color: 'white', fontFamily: 'Ubuntu'}}> Admin </h2>              
+            </div>
+            <div style={{position: 'absolute', right: '14vw', top:'3.8vh' }}>
+                <h2 onClick = {showTeam} style={{color: 'white', fontFamily: 'Ubuntu'}}> Team </h2>              
+            </div>
 
-        //     <div style={{position: 'absolute', left: '13vw', top:'23vh'}} >
-        //         <img src='./pics/logo_full.png' alt='logo' height={60} />
-        //     </div>
+            {/* continue div */}
+            <div style={{position: 'absolute', right: '3vw', top:'3.5vh' }}>
+                <a href={window.location.href + 'login'}>
+                    <Button variant="contained" className={classes.button}>
+                        Get Start
+                    </Button>
+                </a>
+
+            </div>
+            
+            {/* slogon div */}
+            <div align = 'center' style={{position: 'absolute', left: '41vw', top:'30vh' }}>
+                <h1 style={{color: 'white', fontFamily: 'Ubuntu'}}> New-Bee Personal CRM </h1>
+                <h1 style={{color: 'white', fontFamily: 'Ubuntu'}}> Admin Version </h1>              
+            </div>
+
+            <Drawer
+                title="About"
+                placement={'left'}
+                onClose={onClose}
+                visible={about}
+                key = {'d1'}
+            >
+                <p>This is NEW-BEE Personal CRM, developed by team New-Bees from University of Melbourne.</p>
+                <br/>
+                <p>We offer free CRM service to small business, private business, local vendor and freelancer.</p>
+                <br/>
+                <p>Our priorities are build friendly community that users can connect with other users easily and comfortably.</p>
+                <br/>
+                <p>The New-Bees team reserves the right of final interpretation of this CRM.</p>
+            </Drawer>
+
+            <Drawer
+                title="Support"
+                placement={'left'}
+                onClose={onClose}
+                visible={support}
+                key = {'d2'}
+            >
+                <p>The New-Bees Team is glad to offer support with your account.</p>
+                <br/>
+                <p>Please comply with the internet rules and local laws, and show respect to other users. We do not tolerate racist or other types of discriminaition information/massage. Failed to comply with above terms may lead to your account being banned.</p>
+                <br/>
+                <p>For further assistance, please contact us via the email: ziyuq@student.unimelb.edu.au.</p>
+            </Drawer>
+
+            <Drawer
+                title="Admin"
+                placement={'left'}
+                onClose={onClose}
+                visible={admin}
+                key = {'d3'}
+            >
+                <p>Administrators of NEW-BEE CRM are amicable partners that help New-Bees Team to maintain a friendly online community.</p>
+                <br/>
+                <p>Admins reserve the rights of banning and unbanning any accounts that violate the community rules. Admins also reserves the right of modifying users information, but we guarantee that users private information will not be revealed to any third parties.</p>
+                <br/>
+                <p>For appeal of banned accounts please contact us via the email: ziyuq@student.unimelb.edu.au.</p>
+                <br/>
+                <p>For current admins: Please log in with a different website!</p>
+            </Drawer>
+
+            <Drawer
+                title="Team"
+                placement={'left'}
+                onClose={onClose}
+                visible={team}
+                key = {'d4'}
+            >
+                <p>This personal CRM is developed by the student team New-Bees</p>
+                <br/>
+                <p>The Team members are: Shenggang Qian, Shengjun Su, Xinyu Wu, Yutong Sun and Ziyu Qian. The supervisor of the team is Zhe Wang and the client is Haichao Song </p>
+                <br/>
+                <p>For any suggestions and potential future opportunities, please contact us via the email: ziyuq@student.unimelb.edu.au.</p>
+            </Drawer>
+
+
+        </div>
     )
 
 };
 
 export default Homepage;
-
--- admin-front/src/pages/login.js
+++ admin-front/src/pages/login.js
@@ -1,7 +1,7 @@
 //import libraries
 import React from 'react';
 import { useState } from 'react'
-import { message } from 'antd';
+import { message, Drawer } from 'antd';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
@@ -147,7 +147,7 @@ const useStyles = makeStyles((theme) => ({
 // signin page
 function SignIn(props) {
     const classes = useStyles();
-    const [account, setaccount] = useState('');
+    const [account, setAccount] = useState('');
     const [password, setPassword] = useState('');
 
     const onSignIn = () => {
@@ -172,31 +172,58 @@ function SignIn(props) {
         })
     };
 
+    const [support, setSupport] = useState(false);
+
+    const showSupport = () => {
+        setSupport(true);
+    };
+    const onClose = () => {
+        setSupport(false);
+    };
+
 
 
 
     return (
         <div className={classes.background} style={{ width: '100vw', height: '100vh, maxWidth: 100%', margin: '0', overflow: 'hidden' }}>
+            <div style={{position: 'absolute', left: '19vw', top:'4.3vh' }}>
+                <h2 onClick = {showSupport} style={{color: 'black', fontFamily: 'Ubuntu'}}> Support </h2>              
+            </div>
+
+            <Drawer
+                title="Support"
+                placement={'right'}
+                onClose={onClose}
+                visible={support}
+                key = {'d2'}
+            >
+                <p>Welcome to the admin community! Let's maintain a friendly CRM environment together </p>
+                <br/>
+                <p>Please manage the users according to the rules of our website. Do not hesitate to ban them if their activities are offensive </p>
+                <br/>
+                <p>For further clarification, please contact us via the email: ziyuq@student.unimelb.edu.au.</p>
+            </Drawer>
+
+            
 
             <div className={classes.middle}>
                 <Container component="main" maxWidth="xs">
+
                     <a href="/">
-                        <img src='../pics/logo_full.png' alt="logo pic" style={{ width: '100%', verticalAlign: 'middle' }}></img>
+                        <img src='../pics/logo_full.png' alt="logo pic" style={{position: 'absolute', left: '3vw', top:'3vh', height: '50px' }}></img>
                     </a>
-                    <br />
-                    <br />
+ 
                     <div>
 
-                        <Typography component="h1" variant="h3" align='center'>Admin Log In</Typography>
+                        <Typography component="h1" variant="h3" align='center' style = {{fontFamily: 'Ubuntu'}}>Admin Log In</Typography>
                     </div>
-                    <br />
+
                     <div>
 
-                        <p align='center'>Who controls the past controls the future.</p>
-                        <p align='center'>Who controls the present controls the past.</p>
-                        <p align='right'>― George Orwell, 1984</p>
+                        <p align='center' style = {{fontFamily: 'Ubuntu'}}>Who controls the past controls the future.</p>
+                        <p align='center' style = {{fontFamily: 'Ubuntu'}}>Who controls the present controls the past.</p>
+                        <p align='right' style = {{fontFamily: 'Ubuntu'}}>― George Orwell, 1984</p>
                     </div>
-                    <br />
                     <div>
                         <form noValidate>
                             <TextField
@@ -209,7 +236,7 @@ function SignIn(props) {
                                 name="account"
                                 autoComplete="account"
                                 autoFocus
-                                onChange={e => setaccount(e.target.value)}
+                                onChange={e => setAccount(e.target.value)}
                             />
 
                             <TextField
